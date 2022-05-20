9ac44c1e15d255e910a072423a81e0e8be7aa2bd
root <root@master>
2022-05-20 08:24:33 +0000

    2022-05-20

--- rabbitmq-to-faceDetectedMQ/faceDetect.py
+++ rabbitmq-to-faceDetectedMQ/faceDetect.py
@@ -1,5 +1,6 @@
 import sys 
 import cv2
+import face_recognition
  
 inputFile = sys.argv[1]  
 outputFile = sys.argv[2]
@@ -8,6 +9,16 @@ outputFile = sys.argv[2]
 
 im = cv2.imread(inputFile)
 
-im = cv2.bitwise_not(im)
+######
+
+#im = cv2.bitwise_not(im)
+
+imRGB = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
+facePosition = face_recognition.face_locations(imRGB,model='cnn')
+
+for (top,right,bottom,left) in facePosition:
+    cv2.rectangle(im,(left,top),(right,bottom),(0,0,255),3)
+
+######
 
 cv2.imwrite(outputFile, im)
